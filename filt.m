62249c455d9d0c66dea0b19ce767444b7ad5219d
Jacek Anaszewski <j.anaszewski@samsung.com>
2016-02-14 18:19:14 +0100

    convolution working

--- filt.m
+++ filt.m
@@ -20,26 +20,35 @@ N=length(x);
 base=zeros(1,N);
 for (k=201:N)
    base(k) = sig1(x(k));
+   printf("base(%d): %.2f\n", k, base(k));
 endfor;
 
 filt=zeros(1,N);
 for (k=201:N)
-   filt(k) = sig2(x(k));
+#   filt(k) = sig2(x(k));
+#   filt(201-(k-201)) = sig2(x(k));
+   filt(402-k) = sig2(x(k));
+   printf("filt(%d): %.2f\n", k, filt(k));
 endfor;
 
 y=zeros(1,N);
 for (n=201:N)
+   printf("----n=%d-----\n", n);
    for (k=201:N)
-      printf("n: %.2f, k: %.2f, diff: %.2f, y(n): %.2f\n", n, k, 201+n-k, y(n));
       y(n) = y(n) + (base(k) * filt(201+n-k));
+#      printf("base(%d): %.2f, filt(%d): %.2f, y(%d): %.2f\n", k, base(k), 201+n-k, filt(201+n-k), n, y(n));
    endfor;
-   y(n) = y(n) / (N - 201);
+#   y(n) = y(n) / (N - 201);
 endfor;
 
-subplot(3, 1, 1);
+z = conv(base, filt, "same");
+
+subplot(4, 1, 1);
 plot(x,base);
-subplot(3, 1, 2)
+subplot(4, 1, 2)
 plot(x,filt);
-subplot(3, 1, 3);
+subplot(4, 1, 3);
 plot(x,y);
+subplot(4, 1, 4);
+plot(x,z);
 
